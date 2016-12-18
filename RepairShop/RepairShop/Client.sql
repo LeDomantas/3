66b159daadd3dd3bf6ec9acfd4931baa217c0ae9
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2016-12-18 22:23:03 +0200

    Added client table.

---
+++ RepairShop/RepairShop/Client.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Client]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(30) NOT NULL, 
+    [LastName] NVARCHAR(30) NOT NULL, 
+    [Phone] NCHAR(12) NOT NULL, 
+    [Email] NVARCHAR(50) NULL
+)
