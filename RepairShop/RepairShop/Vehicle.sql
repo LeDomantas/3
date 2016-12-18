1ba88ca77d7bca5fe6e0bb59cad2abbb0a8754f4
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2016-12-18 22:13:52 +0200

    Updated Vehicle table.

--- RepairShop/RepairShop/Vehicle.sql
+++ RepairShop/RepairShop/Vehicle.sql
@@ -3,5 +3,6 @@
 	[ID] INT NOT NULL PRIMARY KEY, 
     [VIN] NCHAR(17) NOT NULL, 
     [Color] NCHAR(10) NULL, 
-    [OwnerID] NCHAR(10) NOT NULL
+    [OwnerID] NCHAR(10) NOT NULL, 
+    [EntryDate] DATE NOT NULL
 )
