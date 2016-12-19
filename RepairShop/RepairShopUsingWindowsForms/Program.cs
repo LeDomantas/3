f6ee24c2907b26cc443a47368639535530183f03
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2016-12-19 22:35:34 +0200

    Added a project for lecturer's code testing.

--- RepairShop/RepairShopUsingWindowsForms/Program.cs
+++ RepairShop/RepairShopUsingWindowsForms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,6 +18,8 @@ namespace RepairShopUsingWindowsForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            DataTable table = new DataTable("Client");
         }
     }
 }
